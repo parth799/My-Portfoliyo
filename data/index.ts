f29021211ee240bfe6e490a36f0d6cb755fdc044
parth799 <parthdesai718@gmail.com>
2026-01-08 20:49:51 +0530

    update exprince

--- data/index.ts
+++ data/index.ts
@@ -38,7 +38,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Full Stack Developer with 3+ years of experience building scalable web applications",
+    title: "Full Stack Developer with 2+ years of experience building scalable web applications",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
